5e5a225cb95378504de336c01443aab17f0c9496
Vimal Jobanputra <vim@bitbybit.io>
2013-08-27 21:30:30 +1200

    Log changes between revisions too

--- deploy_notification/recipes/default.rb
+++ deploy_notification/recipes/default.rb
@@ -7,8 +7,9 @@ require 'hipchat'
 node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Getting commit info for app #{application}")
-  current_dir = "#{deploy[:deploy_to]}/current"
   stack_name = node[:stack][:name]
+  current_dir = "#{deploy[:deploy_to]}/current"
+  previous_deploy_dir =  Dir.entries("#{deploy[:deploy_to]}/releases").sort[-2]
 
 	commit_sha_cmd = Mixlib::ShellOut.new('git log -1 --format="%h"', {
 		:cwd => current_dir
@@ -16,23 +17,35 @@ node[:deploy].each do |application, deploy|
 	commit_sha_cmd.run_command
 	commit_sha = commit_sha_cmd.stdout
 
+	previous_commit_sha_cmd = Mixlib::ShellOut.new('git log -1 --format="%h"', {
+		:cwd => previous_deploy_dir
+	})
+	previous_commit_sha_cmd.run_command
+	previous_commit_sha = previous_commit_sha_cmd.stdout
+
+	changes_cmd = Mixlib::ShellOut.new("git log #{commit_sha}...#{previous_commit_sha} --format=\"[%h] %an: %s\"", {
+		:cwd => current_dir
+	})
+	changes_cmd.run_command
+	changes = changes_cmd.stdout
+
 	commit_branch_cmd = Mixlib::ShellOut.new('git rev-parse --abbrev-ref HEAD', {
 		:cwd => current_dir
 	})
 	commit_branch_cmd.run_command
 	commit_branch = commit_branch_cmd.stdout
 
-	Chef::Log.info("Sending commit info to hipchat for app #{application}")
-	message = "Commit #{commit_sha} from branch '#{commit_branch}' was deployed to #{stack_name}"
-	client = HipChat::Client.new('c9bd281c9f01e292cbbb8fe3199fa6')
-	client['PosBoss'].send('Deploy', message, :color => 'red')
+	# Chef::Log.info("Sending commit info to hipchat for app #{application}")
+	# message = "Commit #{commit_sha} from branch '#{commit_branch}' was deployed to #{stack_name}"
+	# client = HipChat::Client.new('c9bd281c9f01e292cbbb8fe3199fa6')
+	# client['PosBoss'].send('Deploy', message, :color => 'red')
 
 	Chef::Log.info("Writing build info file for app #{application}")
   file "#{current_dir}/public/buildinfo.txt" do
     group deploy[:group]
     owner deploy[:user]
     mode 0775
-    content "#{Time.now.to_s}\n#{commit_branch}\n#{commit_sha}"
+    content "#{Time.now.to_s}\n#{commit_branch}\n#{commit_sha}\n#{changes}"
     action :create
   end
 end
