e66fa2810f3b0566052c3c5387b4add578cb13b3
Alex Li <alex.zige@gmail.com>
2014-10-20 17:50:33 +1300

    Fix set env vars to shell

--- env_vars/recipes/default.rb
+++ env_vars/recipes/default.rb
@@ -1,13 +1,14 @@
 node[:deploy].each do |application, deploy|
   Chef::Log.info("Deploy for application #{application} as #{deploy[:application_type]} app")
 
-  node.each do | key, value |
-    Chef::Log.info( "#{key} = #{value}" )
-  end
-
+  #Get ENV VARS from new opsworks stupid UI
   environment_variables = deploy[:environment_variables]
-  Chef::Log.info("deploy: #{deploy}")
+
   Chef::Log.info("Environment: #{environment_variables}")
+
+  execute "SET ENV VARS TO SHELL" do
+    environment(environment_variables)
+  end
 
 end
 
